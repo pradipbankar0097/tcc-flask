f34781d4dbf628462633f0fc9e0439a94181d9b5
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-12-07 14:13:49 +0530

    for testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from scout_apm.flask import ScoutApm
 
 app = Flask(__name__)
+
+
+# Setup a flask 'app' as normal
+
+# Attach ScoutApm to the Flask App
+ScoutApm(app)
+
+# Scout settings
+app.config["SCOUT_MONITOR"] = True
+app.config["SCOUT_KEY"] = "S12c7PD0IlegD8qwEBRk"
+app.config["SCOUT_NAME"] = "TCC"
+# If you'd like to utilize Error Monitoring:
+app.config["SCOUT_ERRORS_ENABLED"] = True
+
 
 if app.config["ENV"] == "production":
     app.config.from_object("config.DevelopmentConfig")
